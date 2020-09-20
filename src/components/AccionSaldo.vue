f7eec5a4fac0ae386de643165add2b57b72ee3d6
Martin Barria Joost <martinjoostw@gmail.com>
2020-09-20 17:56:41 -0600

    feat(action): component

---
+++ src/components/AccionSaldo.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="action">{{txt}}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    txt: String,
+  },
+  methods: {
+    action() {
+      this.$emit("action");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/components/Cuenta.vue
+++ src/components/Cuenta.vue
@@ -1,9 +1,35 @@
 <template>
-  <h1>Cuenta</h1>
+  <h1>Saldo: {{summary}}</h1>
+  <h1>Tipo de cuenta: {{type}}</h1>
+  <h1>cuenta: {{state ? 'Activa' : 'Descativada'}}</h1>
+  <div v-for="(servicio, index) in servicios" :key="index">{{index + 1}} - {{servicio}}</div>
+  <AccionSaldo txt="Aumentar Saldo" @action="increment" />
+  <AccionSaldo txt="Restar Saldo" @action="decrement" />
 </template>
 
 <script>
-export default {};
+import AccionSaldo from "./AccionSaldo";
+export default {
+  data() {
+    return {
+      summary: 1000,
+      type: "visa",
+      state: true,
+      servicios: ["giro", "transferencia", "abono"],
+    };
+  },
+  components: {
+    AccionSaldo,
+  },
+  methods: {
+    increment() {
+      this.summary = this.summary + 100;
+    },
+    decrement() {
+      this.summary = this.summary - 100;
+    },
+  },
+};
 </script>
 
 <style>
